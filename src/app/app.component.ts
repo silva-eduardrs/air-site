add6b968b0d34b1cb4e599f3f8e019145bd49eed
Eduardo Silva <eduardo.r.da.silva@accenture.com>
2024-08-06 23:47:32 -0300

    setup e tratamento scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,102 @@
-import { Component } from '@angular/core';
+import { CarouselModule } from 'primeng/carousel';
+
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, CarouselModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'air-site';
+
+  responsiveCarouselOptions = [
+    {
+      breakpoint: '768px',
+      numVisible: 1,
+      numScroll: 1,
+    },
+    {
+      breakpoint: '1024px',
+      numVisible: 2,
+      numScroll: 1,
+    },
+  ];
+  brandsCarousel = [
+    {
+      image: 'images/logo-gree.png',
+      name: 'Logo Gree',
+    },
+    {
+      image: 'images/logo-daikin.png',
+      name: 'Logo Daikin',
+    },
+    {
+      image: 'images/logo-samsung.png',
+      name: 'Logo Samsung',
+    },
+    {
+      image: 'images/logo-lg.png',
+      name: 'Logo LG',
+    },
+    {
+      image: 'images/logo-midea.png',
+      name: 'Logo Midea',
+    },
+    {
+      image: 'images/logo-carrier.png',
+      name: 'Logo Carrier',
+    },
+  ];
+  innerWidth = window.innerWidth;
+  innerHeight = window.innerHeight;
+  scrollPage = 'transform: translate3d(0px, 0px, 0px);';
+  heightScrolled = 0;
+  section = 0; //
+
+  ngOnInit(): void {
+    document.addEventListener('mousewheel',(event) => this.onScroll(event),false);
+    document.addEventListener('DOMMouseScroll',(event) => this.onScroll(event),false);
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'ArrowDown' || event.key === 'ArrowRight' || event.key === 'Space' || event.key === 'PageDown') {
+        this.scrollDown();
+      } else if (event.key === 'ArrowUp' || event.key === 'ArrowLeft' || event.key === 'PageUp') {
+        this.scrollDown();
+      }
+    }, false);
+  }
+
+  onScroll(event: any) {
+    event.deltaY > 0 ? this.scrollDown() : this.scrollUp();
+  }
+
+  scrollDown() {
+    if (this.section <= 3) {
+      this.heightScrolled += this.innerHeight;
+      this.section += 1;
+    }
+    this.setScrollPage();
+  }
+
+  scrollUp() {
+    if (this.section > 0) {
+      this.heightScrolled -= this.innerHeight;
+      this.section -= 1;
+    }
+    this.setScrollPage();
+  }
+
+  scrollToSection(section: number) {
+    this.section = section;
+    this.heightScrolled = this.innerHeight * section;
+    this.setScrollPage();
+  }
+
+  setScrollPage() {
+    this.scrollPage = `transform: translate3d(0px, -${this.heightScrolled}px, 0px);`;
+  }
 }
